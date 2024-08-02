ab70b920f196b84c6ef655b6c170668dbcfbfcd0
jetsl <slkireev@yandex.ru>
2024-08-02 23:07:31 +0700

    Update index.py

--- index.py
+++ index.py
@@ -1,1 +1,20 @@
-print('Hello world!')
+import random
+
+A=[random.randint(-100, 100) for r in range(10)]
+print(f'A = {A}')
+
+def find_sum(A):
+    b=A.index(min(A))
+    c=A.index(max(A))
+    if c<b:
+        c,b=b,c
+    print(f'extremum {A[b]} and {A[c]}')
+    A=A[b+1:c]
+    print(f'list between min and max {A}')
+    total=0
+    for i in A:
+        if i<0:
+            total+=i
+    print(f'Sum of negative = {total}')
+
+find_sum(A)
